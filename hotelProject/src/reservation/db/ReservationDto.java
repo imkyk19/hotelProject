f61e3b8e89e74b7016fca2cff0938c4ec39f750c
bitcamp <bitcamp@DESKTOP-14DI0F5>
2021-05-04 11:58:57 +0900

    room dto & reservation dto

--- hotelProject/src/reservation/db/ReservationDto.java
+++ hotelProject/src/reservation/db/ReservationDto.java
@@ -1,5 +1,72 @@
 package reservation.db;
 
-public class ReservationDto {
+import java.sql.Timestamp;
 
+public class ReservationDto {
+	private int num;
+	private int guestNum;
+	private String guestName;
+	private Timestamp checkInDate;
+	private Timestamp checkOutDate;
+	private int guestQty;
+	private int bookingQty;
+	private int totalPrice;
+	private int roomNum;
+	
+	public int getRoomNum() {
+		return roomNum;
+	}
+	public void setRoomNum(int roomNum) {
+		this.roomNum = roomNum;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getGuestNum() {
+		return guestNum;
+	}
+	public void setGuestNum(int guestNum) {
+		this.guestNum = guestNum;
+	}
+	public String getGuestName() {
+		return guestName;
+	}
+	public void setGuestName(String guestName) {
+		this.guestName = guestName;
+	}
+	public Timestamp getCheckInDate() {
+		return checkInDate;
+	}
+	public void setCheckInDate(Timestamp checkInDate) {
+		this.checkInDate = checkInDate;
+	}
+	public Timestamp getCheckOutDate() {
+		return checkOutDate;
+	}
+	public void setCheckOutDate(Timestamp checkOutDate) {
+		this.checkOutDate = checkOutDate;
+	}
+	public int getGuestQty() {
+		return guestQty;
+	}
+	public void setGuestQty(int guestQty) {
+		this.guestQty = guestQty;
+	}
+	public int getBookingQty() {
+		return bookingQty;
+	}
+	public void setBookingQty(int bookingQty) {
+		this.bookingQty = bookingQty;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+	
 }
--- hotelProject/src/room/db/RoomDto.java
+++ hotelProject/src/room/db/RoomDto.java
@@ -1,5 +1,56 @@
 package room.db;
 
 public class RoomDto {
-
+	private int num;
+	private int roomNum;
+	private String status;
+	private int capacity;
+	private int initialQty;
+	private int availableQty;
+	private int price;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getRoomNum() {
+		return roomNum;
+	}
+	public void setRoomNum(int roomNum) {
+		this.roomNum = roomNum;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public int getInitialQty() {
+		return initialQty;
+	}
+	public void setInitialQty(int initialQty) {
+		this.initialQty = initialQty;
+	}
+	public int getAvailableQty() {
+		return availableQty;
+	}
+	public void setAvailableQty(int availableQty) {
+		this.availableQty = availableQty;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
 }
